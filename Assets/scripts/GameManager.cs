94352d75ac4b8ab8d30241102496dfd266949b10
Mansur Oralov <oralov@sheridancollege.ca>
2020-06-01 11:58:02 -0400

    c

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState
+{
+    menu,
+    inGame,
+    gameOver
+}
+
+public class GameManager : MonoBehaviour
+{
+    public GameState currentGameState = GameState.menu;
+    public static GameManager instance;
+    public Canvas menuCanvas;
+    public Canvas inGameCanvas;
+    public Canvas gameOverCanvas;
+
+
+
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+
+   
+
+    void Start()
+    {
+        //StartGame();
+        }
+
+// called to start the game
+    public void StartGame()
+    {
+        SetGameState(GameState.inGame);
+        PlayerControler.instance.Start();
+        SetGameState(GameState.inGame);
+    }
+    // called when player dies
+    public void GameOver()
+    {
+        SetGameState(GameState.gameOver);
+    }
+
+    public void BackToMenu()
+    {
+        SetGameState(GameState.menu);
+    }
+
+    
+
+    void Update()
+    {
+        if (Input.GetButtonDown("s"))
+        {
+            StartGame();
+            Debug.Log("lohhhh");
+        }
+    }
+
+    void SetGameState(GameState newGameState)
+    {
+        if (newGameState == GameState.menu)
+        {
+            menuCanvas.enabled = true;
+            inGameCanvas.enabled = false;
+            gameOverCanvas.enabled = false;
+        }
+        else if(newGameState == GameState.inGame)
+        {
+            menuCanvas.enabled = false;
+            inGameCanvas.enabled = true;
+            gameOverCanvas.enabled = false;
+
+        }
+        else if(newGameState == GameState.gameOver)
+        {
+            menuCanvas.enabled = false;
+            inGameCanvas.enabled = false;
+            gameOverCanvas.enabled = true;
+
+        }
+        currentGameState = newGameState;
+    }
+    
+}
---
+++ Assets/scripts/PlayerControler.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerControler : MonoBehaviour
+{
+
+    public static PlayerControler instance;
+    public float jumpForce = 6f;
+    private Rigidbody2D rigidBody;
+    public Animator animator;
+    public float runningSpeed = 1.5f;
+    public Vector3 startingPosition;
+
+
+
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        instance = this;
+        startingPosition = this.transform.position;
+    }
+
+
+    // Start is called before the first frame update
+  public  void Start()
+    {
+        animator.SetBool("isAlive", true);
+        this.transform.position = startingPosition;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.instance.currentGameState == GameState.inGame)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Jump();
+                Debug.Log("loh");
+
+            }
+
+
+            animator.SetBool("isGrounded", IsGrounded());
+        }
+    }
+
+
+    void Jump()
+    {
+        if (IsGrounded())
+        {
+            rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    public LayerMask groundLayer;
+
+    bool IsGrounded()
+    {
+
+        if (Physics2D.Raycast(this.transform.position, Vector2.down, 0.3f, groundLayer.value))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (GameManager.instance.currentGameState == GameState.inGame)
+        {
+            if (rigidBody.velocity.x < runningSpeed)
+            {
+                rigidBody.velocity = new Vector2(runningSpeed, rigidBody.velocity.y);
+            }
+
+        }
+    }
+
+
+    public void Kill()
+    {
+        GameManager.instance.GameOver();
+        animator.SetBool("isAlive", false);
+    }
+} 
